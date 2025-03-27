79eca3304149fe54f7f3a8bfd4ee8448d64d1daf
Glen Langer <contao.modul@glen-langer.de>
2025-03-28 00:39:18 +0100

    #3, Korrektur Pfad services.yml

--- src/DependencyInjection/BugBusterLastloginExtension.php
+++ src/DependencyInjection/BugBusterLastloginExtension.php
@@ -23,7 +23,7 @@ class BugBusterLastloginExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
         $loader->load('services.yaml');
     }
 }
