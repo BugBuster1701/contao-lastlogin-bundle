fab0d628501f513f0ef4ce1c350217abde9a2bde
Glen Langer <contao.modul@glen-langer.de>
2025-05-07 22:14:46 +0200

    #12, ECS Anpassungen

--- tools/ecs/config/default.php
+++ tools/ecs/config/default.php
@@ -23,16 +23,6 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([__DIR__.'/../vendor/contao/easy-coding-standard/config/contao.php']);
 
-    $ecsConfig->skip([
-        'src/Resources/contao/*',
-        // MethodChainingIndentationFixer::class => [
-        //     '*/DependencyInjection/Configuration.php',
-        // ],
-        TrailingCommaInMultilineFixer::class => [
-            'src/Controller/FrontendModule/VisitorsFrontendController.php',
-        ]
-    ]);
-
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Lastlogin\n@link       https://github.com/BugBuster1701/contao-lastlogin-bundle\n\n@license    LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.",
     ]);
